4d5dfd1ab4e40bac0716c0a0b8aaebb0eabc9672
anjaligopi <anjaligopi23@gmail.com>
2020-10-01 12:16:55 -0700

    test multiple cases using markdown decorator

--- sample-pytest.py
+++ sample-pytest.py
@@ -3,9 +3,19 @@ import pytest
 def func(x):
     return x + 5
 
-def test_method():
+# to test multiple test cases, use this mark decorator
+@pytest.mark.test1
+def test_method1():
     assert func(3) == 8
+
+@pytest.mark.test2
+def test_method2():
     assert func(3) == 11
     
-if __name__ == "__main__":
-    test_method()
+# Run iusing pytest sample-pytest.py -m test1
+
+# if __name__ == "__main__":
+#     # test_method1()
+#     # test_method2()
+#     # func(8)
+#     pass
