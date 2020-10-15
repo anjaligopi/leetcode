0b8cd5951788abe2cf4b475b21a2fa7b52c07cef
anjaligopi <anjaligopi23@gmail.com>
2020-10-15 15:18:01 -0700

    add rotate array 189 with pytest

--- daily_coding_challenge/october_2020/rotate_array_189.py
+++ daily_coding_challenge/october_2020/rotate_array_189.py
@@ -28,6 +28,7 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         k = k % len(nums)
         nums[:] = nums[-k:] + nums[:-k]
+        # nums[:] = nums[len(nums)-k : ] + nums[ 0 : len(nums)-k ]
         return nums
             
 @pytest.mark.timeout(3)
